de409d52c36d022b83458fbb563f0a133d7bf4ab
Binod Pokhrel <lilbp221@gmail.cok>
2024-07-31 20:36:50 +0545

    DB CHANGED TO RAILWAY

--- model/index.js
+++ model/index.js
@@ -1,18 +1,27 @@
 const dbConfig= require('../config/dbConfig')
 const {Sequelize,DataTypes}=require('sequelize')
 
-const sequelize= new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,{
-      host:dbConfig.HOST,
-      dialect:dbConfig.dialect,
-      // operatorsAliases:false,
-      port:3306,
-      pool:{
-            max:dbConfig.max,
-            min:dbConfig.min,
-            acquire:dbConfig.acquire,
-            idle:dbConfig.idle,
+// const sequelize= new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,{
+//       host:dbConfig.HOST,
+//       dialect:dbConfig.dialect,
+//       // operatorsAliases:false,
+//       port:3306,
+//       pool:{
+//             max:dbConfig.max,
+//             min:dbConfig.min,
+//             acquire:dbConfig.acquire,
+//             idle:dbConfig.idle,
+//       }
+// })
+const sequelize = new Sequelize('mysql://root:tfMaLkIVWOgKxftZmJgzrMFCJrPrJpQK@monorail.proxy.rlwy.net:17435/railway', {
+      dialect: 'mysql',
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
       }
-})
+    });
 
 sequelize.authenticate().
 then( ()=>{
@@ -39,3 +48,5 @@ db.sequelize.sync({force:false}).then(()=>{
 //this method syncs all the defined model to database. {force:true} drops the table if already exists so we user false value
 
 module.exports=db;
+
+
